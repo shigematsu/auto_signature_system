82be185ade64532ef7a0d908795c4591b3d9e69c
shigematsu <sigematu@sfc.wide.ad.jp>
2011-11-15 20:42:05 +0900

    Firsit commit

---
+++ bk/packetdb.rb
@@ -0,0 +1,77 @@
+#!/usr/bin/env ruby -wKU
+
+require 'rubygems'
+require 'sqlite3'
+require 'pcap'
+
+DBFILENAME = "/home/shigematsu/db/packetdb.sqlite3"
+
+DBDIR = File.dirname( DBFILENAME )
+DBNAME = File.basename( DBFILENAME )
+
+Dir.chdir( DBDIR )
+db = SQLite3::Database.new( DBFILENAME )
+
+sql = <<SQL
+# DROP TABLE tcppackets;
+SQL
+puts "drop table tcppackets..."
+
+# db.execute_batch(sql)
+
+sql = <<SQL
+# DROP TABLE udppackets;
+SQL
+puts "drop table udppackets..."
+
+# db.execute_batch(sql)
+
+sql = <<SQL
+CREATE TABLE tcppackets (
+    id            INTEGER  PRIMARY KEY AUTOINCREMENT,
+    time          REAL NOT NULL,
+    size          INTEGER NOT NULL,
+    tcp_data_len  INTEGER NOT NULL, 
+    ip_src        INTEGER NOT NULL,
+    tcp_sport     INTEGER NOT NULL,
+    ip_dst        INTEGER NOT NULL,
+    tcp_dport     INTEGER NOT NULL,
+    tcp_flags_s   TEXT NOT NULL,
+    tcp_seq       INTEGER NOT NULL );
+CREATE TABLE udppackets (
+    id            INTEGER  PRIMARY KEY AUTOINCREMENT,
+    time          REAL NOT NULL,
+    size          INTEGER NOT NULL,
+    udp_len  INTEGER NOT NULL, 
+    ip_src        INTEGER NOT NULL,
+    udp_sport     INTEGER NOT NULL,
+    ip_dst        INTEGER NOT NULL,
+    udp_dport     INTEGER NOT NULL );  
+SQL
+puts "define table tcppackets..."
+db.execute_batch(sql)
+
+puts "insert values..."
+
+db.transaction do
+
+
+File::open("/home/list/filelist") {|file|
+  while malpcap = file.gets
+    filename = malpcap.chomp
+    puts filename
+
+cap = Pcap::Capture.open_offline(filename)
+cap.setfilter("ip")
+cap.loop do |pkt|
+  if pkt.ip? and pkt.tcp?
+    db.execute( " insert into tcppackets values ( null, ?, ?, ?, ?, ?, ?, ?, ?, ? )", pkt.time.to_f, pkt.size, pkt.tcp_data_len, pkt.ip_src, pkt.tcp_sport, pkt.ip_dst, pkt.tcp_dport, pkt.tcp_flags_s, pkt.tcp_seq )
+  end
+  if pkt.ip? and pkt.udp?
+    db.execute( " insert into udppackets values ( null, ?, ?, ?, ?, ?, ?, ? )", pkt.time.to_f, pkt.size, pkt.udp_len, pkt.ip_src, pkt.udp_sport, pkt.ip_dst, pkt.udp_dport )
+  end
+end
+cap.close
+end
+}
+end
---
+++ packetdb_normal.rb
@@ -0,0 +1,79 @@
+#!/usr/bin/env ruby -wKU
+
+require 'rubygems'
+require 'sqlite3'
+require 'pcap'
+
+DBFILENAME = "/home/shigematsu/db/packetdb.sqlite3"
+
+DBDIR = File.dirname( DBFILENAME )
+DBNAME = File.basename( DBFILENAME )
+
+Dir.chdir( DBDIR )
+db = SQLite3::Database.new( DBFILENAME )
+
+sql = <<SQL
+# DROP TABLE tcppackets;
+SQL
+puts "drop table tcppackets_normal..."
+
+# db.execute_batch(sql)
+
+sql = <<SQL
+# DROP TABLE udppackets;
+SQL
+puts "drop table udppackets_normal..."
+
+# db.execute_batch(sql)
+
+sql = <<SQL
+CREATE TABLE tcppackets_normal (
+    id            INTEGER  PRIMARY KEY AUTOINCREMENT,
+    time          REAL NOT NULL,
+    size          INTEGER NOT NULL,
+    tcp_data_len  INTEGER NOT NULL, 
+    ip_src        INTEGER NOT NULL,
+    tcp_sport     INTEGER NOT NULL,
+    ip_dst        INTEGER NOT NULL,
+    tcp_dport     INTEGER NOT NULL,
+    tcp_flags_s   TEXT NOT NULL,
+    tcp_seq       INTEGER NOT NULL,
+    tcp_data      TEXT );
+CREATE TABLE udppackets_normal (
+    id            INTEGER  PRIMARY KEY AUTOINCREMENT,
+    time          REAL NOT NULL,
+    size          INTEGER NOT NULL,
+    udp_len  INTEGER NOT NULL, 
+    ip_src        INTEGER NOT NULL,
+    udp_sport     INTEGER NOT NULL,
+    ip_dst        INTEGER NOT NULL,
+    udp_dport     INTEGER NOT NULL,
+    udp_data      TEXT );  
+SQL
+puts "define table tcppackets..."
+db.execute_batch(sql)
+
+puts "insert values..."
+
+db.transaction do
+
+
+File::open("/home/shigematsu/list/filelist_normal") {|file|
+  while malpcap = file.gets
+    filename = malpcap.chomp
+    puts filename
+
+cap = Pcap::Capture.open_offline(filename)
+cap.setfilter("ip")
+cap.loop do |pkt|
+  if pkt.ip? and pkt.tcp?
+    db.execute( " insert into tcppackets_normal values ( null, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", pkt.time.to_f, pkt.size, pkt.tcp_data_len, pkt.ip_src, pkt.tcp_sport, pkt.ip_dst, pkt.tcp_dport, pkt.tcp_flags_s, pkt.tcp_seq, pkt.tcp_data )
+  end
+  if pkt.ip? and pkt.udp?
+    db.execute( " insert into udppackets_normal values ( null, ?, ?, ?, ?, ?, ?, ?, ?)", pkt.time.to_f, pkt.size, pkt.udp_len, pkt.ip_src, pkt.udp_sport, pkt.ip_dst, pkt.udp_dport, pkt.udp_data )
+  end
+end
+cap.close
+end
+}
+end
